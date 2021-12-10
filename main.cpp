b7c0b8785b2e2dcaa9b5fdaca02dcd09c5714477
calebh210 <72399776+calebh210@users.noreply.github.com>
2021-12-10 17:00:10 +0000

    update
    added samples

--- main.cpp
+++ main.cpp
@@ -72,13 +72,35 @@ void fileInput(){
     }
 
 //Lists available sample programs for the simulator to run.
+void fileInput(){
+    int check = 1;
+    string fileName;
+    cout << "Enter a file name to run the simulator" << endl;
+    cin >> fileName;
+
+    if(!exists(fileName)){
+        cout << "Error loading file - file may not exist" << endl;
+        
+    }else{
+        initStore();
+        loadFromFile(fileName);
+        check = run();
+    }
+ 
+    if(check == 0){
+
+        cout << "Program Successful" << endl;
+        
+        }
+    }
+
 void runSamples(){
     int choice;
     cout << "Choose a sample program to run (End result shown in accumulator)" << endl;
     cout << "1. Run Sample 1 (Add 5 + 3)" << endl;
-    cout << "2. Run Sample 2 " << endl;
-    cout << "3. Run Sample 3 " << endl;
-    cout << "4 .Run 'BabyTest1-MC.txt' " << endl;
+    cout << "2. Run Sample 2 ( Add 53 + 789) " << endl;
+    cout << "3. Run 'BabyTest1-MC.txt' " << endl;
+    cout << "4. Exit" << endl;
     cin >> choice;
     switch(choice)
     {
@@ -87,6 +109,22 @@ void runSamples(){
             loadFromFile("Sample1.txt");
             run();
             break;
+        case 2:
+            initStore();
+            loadFromFile("Sample2.txt");
+            run();
+            break;
+        case 3:
+            initStore();
+            loadFromFile("BabyTest1-MC.txt");
+            run();
+            break;
+        case 4:
+            break;
+        default:
+             cout << "Invalid input\n"
+                 << endl;
+            cout << "" << endl;
     }
 
 }
